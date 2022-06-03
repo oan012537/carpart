be80075bc3d3d0276b4616c735fcc386685cc3e2
Boss TeamA <yamchocolate@gmail.com>
2022-06-04 01:40:44 +0700

    Update

--- app/Http/Controllers/Buyer/buyerController.php
+++ app/Http/Controllers/Buyer/buyerController.php
@@ -11,6 +11,11 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
+use Helper;
+use DB;
+use App\Models\Backend\mUsers_buyer;
+
+
 class buyerController extends Controller
 {
     public function login_buyer(){
@@ -19,5 +24,41 @@ class buyerController extends Controller
 
     public function regis_buyer(){
         return view('buyer.regis-buy');
+    }
+    public function regis_buyer_post(Request $request){
+        session
+
+        return view('buyer.regis-buy');
+    }
+
+    public function regiscon_buyer(){
+        return view('buyer.regiscon-buy');
+    }
+
+    public function registerpass_buyer(){
+        return view('buyer.registerpass-buy');
+    }
+
+    public function register(Request $request){
+
+        $data->username = $request->username;
+        $data->password = $request->password;
+        $data->firstname = $request->firstname;
+        $data->lastname = $request->lastname;
+        $data->phone = $request->phone;
+        $data->save();
+
+        if($request->password == $request->confirm_password){
+
+            return view("back-end.alert.success",['url'=> url("/login") ]);
+
+        }else{
+
+            return view("back-end.alert.alert", [
+                'url' => '/register',
+                'title' => "เกิดข้อผิดพลาด",
+                'text' => "Password ไม่ตรงกัน",
+            ]);
+         }
     }
 }
