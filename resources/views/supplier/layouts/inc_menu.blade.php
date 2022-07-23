373a2d7ef0111abc0b7524ebb8dd5319fd368102
Pongpisut <pongpisut1997@gmail.com>
2022-07-24 00:52:11 +0700

    responsive Menu Supplier and Backend

--- resources/views/supplier/layouts/inc_menu.blade.php
+++ resources/views/supplier/layouts/inc_menu.blade.php
@@ -10,7 +10,7 @@
             </a>
         </li>
 
-      
+
         <!--  -->
 
         <li class="nav-item box__icon2">
@@ -43,139 +43,166 @@
             </ul>
         </li>
 
+
+
         <!--  -->
 
-        <li class="header_img"> 
-            <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+        <li class="header_img">
+            <a href="#">
                 <img src="{{asset('backends/assets/img/hczKIze.jpg')}}" alt="profile">
             </a>
+        </li>
+
+        <li onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            <a href="#" class="btn__logout"><i class="fa-solid fa-right-from-bracket"></i> ออกจากระบบ</a>
             <form id="logout-form" action="{{ route('supplier.logout') }}" method="POST" class="d-none">
                 @csrf
             </form>
         </li>
-        
+
+
     </ul>
 
 </header>
 <div class="l-navbar showmenu" id="nav-bar">
+    <div class="box__wrappermenu">
+        <div class="togglemobile">
+            <div class="header_toggle"> <img src="{{asset('assets/img/navbar/icon-richmenu.svg')}}" alt="" id="header-toggle2"> </div>
+        </div>
+
+        <div class="icon__mobile"> <a href="supplier-notiboard.php" class="nav_logo"> <img src="{{asset('assets/img/navbar/logo-whites.svg')}}" class="img-fluid" alt=""> </a></div>
+
+        <div class="nav-item dropdown langmobile">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="{{asset('assets/img/icon/icon-web.svg')}}" class="img-fluid" style="width: 30px" alt=""> {{ trans('file.Language') }}
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="{{ url('language_switch/th') }}">ไทย</a></li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="{{ url('language_switch/en') }}">English</a></li>
+            </ul>
+        </div>
+    </div>
+
     <nav class="nav">
-        <div> <a href="supplier-notiboard.php" class="nav_logo"> <img src="{{asset('assets/img/navbar/logo-whites.svg')}}" class="img-fluid" alt=""> </a>
-            <div class="nav_list">
-                <li>
-                    <a href="supplier-notiboard.php" class="nav_link" data-page="notiboard">
-                        <div class="icon__sidebar1"></div>
-                        <span class="nav_name">Notification <span class="box__iconmenu"> <span class="circle">12</span></span>
-                    </a>
-                </li>
+        <div class="nav_list">
+            <li>
+                <a href="supplier-notiboard.php" class="nav_link" data-page="notiboard">
+                    <div class="icon__sidebar1"></div>
+                    <span class="nav_name">Notification <span class="box__iconmenu"> <span class="circle">12</span></span>
+                </a>
+            </li>
 
-                <li>
-                    <a href="#" class="nav_link">
-                        <div class="icon__sidebar2"></div>
-                        <span class="nav_name">รายการรอยืนยัน<br>ความพร้อมสินค้า <span class="box__iconmenu"> <span class="circle">12</span></span>
-                    </a>
-                </li>
+            <li>
+                <a href="#" class="nav_link">
+                    <div class="icon__sidebar2"></div>
+                    <span class="nav_name">รายการรอยืนยัน<br>ความพร้อมสินค้า <span class="box__iconmenu"> <span class="circle">12</span></span>
+                </a>
+            </li>
 
-                <li>
-                    <a href="#" class="nav_link">
-                        <div class="icon__sidebar3"></div>
-                        <span class="nav_name">รายการสั่งซื้อ <span class="box__iconmenu"> <span class="circle">12</span></span>
-                    </a>
-                </li>
+            <li>
+                <a href="#" class="nav_link">
+                    <div class="icon__sidebar3"></div>
+                    <span class="nav_name">รายการสั่งซื้อ <span class="box__iconmenu"> <span class="circle">12</span></span>
+                </a>
+            </li>
 
-                <li>
-                    {{-- spareparts-card.php --}}
-                    <a href="{{route('supplier.requests')}}" class="nav_link" data-page="spareparts-card">
-                        <div class="icon__sidebar4"></div>
-                        <span class="nav_name">คำขอหาอะไหล่ </span>
-                    </a>
-                </li>
+            <li>
+                {{-- spareparts-card.php --}}
+                <a href="{{route('supplier.requests')}}" class="nav_link" data-page="spareparts-card">
+                    <div class="icon__sidebar4"></div>
+                    <span class="nav_name">คำขอหาอะไหล่ </span>
+                </a>
+            </li>
 
-                <li>
-                    <a href="#" class="nav_link">
-                        <div class="icon__sidebar5"></div>
-                        <span class="nav_name">รายการเคลม <span class="box__iconmenu"> <span class="circle">12</span></span>
-                    </a>
-                </li>
+            <li>
+                <a href="#" class="nav_link">
+                    <div class="icon__sidebar5"></div>
+                    <span class="nav_name">รายการเคลม <span class="box__iconmenu"> <span class="circle">12</span></span>
+                </a>
+            </li>
 
-                <li>
-                    <a href="#" class="nav_link">
-                        <div class="icon__sidebar6"></div>
-                        <span class="nav_name">ระบบการเงิน <span class="box__iconmenu"> <span class="circle">12</span></span>
-                    </a>
-                </li>
-                {{-- inventory --}}
-                <li id="product">
-                    <form id="frm-product" action="{{ route('products.index') }}">
-                        <input type="hidden" name="status_code" value="all">
-                    </form>
-                    <a id="product-list-menu" href="javascript:document.getElementById('frm-product').submit();" class="nav_link">
-                        <div class="icon__sidebar7"></div>
-                        <span class="nav_name">{{ trans('file.Manage Product') }}</span>
-                    </a>
-                </li>
+            <li>
+                <a href="#" class="nav_link">
+                    <div class="icon__sidebar6"></div>
+                    <span class="nav_name">ระบบการเงิน <span class="box__iconmenu"> <span class="circle">12</span></span>
+                </a>
+            </li>
+            {{-- inventory --}}
+            <li id="product">
+                <form id="frm-product" action="{{ route('products.index') }}">
+                    <input type="hidden" name="status_code" value="all">
+                </form>
+                <a id="product-list-menu" href="javascript:document.getElementById('frm-product').submit();" class="nav_link">
+                    <div class="icon__sidebar7"></div>
+                    <span class="nav_name">{{ trans('file.Manage Product') }}</span>
+                </a>
+            </li>
 
-                <!--  นิติบุคคล -->
-                <li>
-                    <a href="supplier-profilelegal.php" class="nav_link" data-page="supplier-profile">
-                        <div class=" icon__sidebar8"></div>
-                        <span class="nav_name">ร้านค้าของฉัน </span>
-                    </a>
-                </li>
+            <!--  นิติบุคคล -->
+            <li>
+                <a href="supplier-profilelegal.php" class="nav_link" data-page="supplier-profile">
+                    <div class=" icon__sidebar8"></div>
+                    <span class="nav_name">ร้านค้าของฉัน </span>
+                </a>
+            </li>
 
-                <!-- ธรรมดา -->
-                <!-- <li>
+            <!-- ธรรมดา -->
+            <!-- <li>
                     <a href="supplier-profile.php" class="nav_link" data-page="supplier-profile">
                         <div class=" icon__sidebar8"></div>
                         <span class="nav_name">ร้านค้าของฉัน </span>
                     </a>
                 </li> -->
 
-                <!--  -->
-                <li>
-                    <a href="#" class="nav_link">
-                        <div class="icon__sidebar9"></div>
-                        <span class="nav_name">โปรโมชั่น </span>
-                    </a>
-                </li>
+            <!--  -->
+            <li>
+                <a href="#" class="nav_link">
+                    <div class="icon__sidebar9"></div>
+                    <span class="nav_name">โปรโมชั่น </span>
+                </a>
+            </li>
 
-                <li>
-                    <a href="#" class="nav_link">
-                        <div class="icon__sidebar10"></div>
+            <li>
+                <a href="#" class="nav_link">
+                    <div class="icon__sidebar10"></div>
 
-                        <span class="nav_name">Chat <span class="box__iconmenu"> <span class="circle">12</span></span>
-                    </a>
-                </li>
+                    <span class="nav_name">Chat <span class="box__iconmenu"> <span class="circle">12</span></span>
+                </a>
+            </li>
 
-                <li>
-                    <a href="#" class="nav_link">
-                        <div class="icon__sidebar11"></div>
+            <li>
+                <a href="#" class="nav_link">
+                    <div class="icon__sidebar11"></div>
 
-                        <span class="nav_name">จัดการขนส่ง </span>
-                    </a>
-                </li>
+                    <span class="nav_name">จัดการขนส่ง </span>
+                </a>
+            </li>
 
-                <li>
-                    <a href="#" class="nav_link">
-                        <div class="icon__sidebar12"></div>
+            <li>
+                <a href="#" class="nav_link">
+                    <div class="icon__sidebar12"></div>
 
-                        <span class="nav_name">รีวิวจากผู้ซื้อ </span>
-                    </a>
-                </li>
+                    <span class="nav_name">รีวิวจากผู้ซื้อ </span>
+                </a>
+            </li>
 
-                <li>
-                    <a href="#" class="nav_link">
-                        <div class="icon__sidebar13"></div>
+            <li>
+                <a href="#" class="nav_link">
+                    <div class="icon__sidebar13"></div>
 
-                        <span class="nav_name">คำถามที่พบบ่อย </span>
-                    </a>
-                </li>
+                    <span class="nav_name">คำถามที่พบบ่อย </span>
+                </a>
+            </li>
 
-                <li>
-                    <a href="#" class="nav_link">
-                        <div class="icon__sidebar14"></div>
-                        <span class="nav_name">Dashboard </span>
-                    </a>
-                </li>
+            <li>
+                <a href="#" class="nav_link">
+                    <div class="icon__sidebar14"></div>
+                    <span class="nav_name">Dashboard </span>
+                </a>
+            </li>
 
 
     </nav>
